9183935a6ef92f33ea6e4ea89c062587e64617e5
Alhamede Emad Eldin <alhamede.abdelgadir@gmail.com>
2017-08-19 14:21:26 +0400

    Create directory 'gradient-descent'

---
+++ gradient-descent/grad_decent.py
@@ -0,0 +1,18 @@
+from numpy import *
+
+def main():
+    #load the data
+    points = genfromtxt('data.csv', delimiter=",")
+
+    #initialize slope, intercept
+    b_init = 0
+    m_init = 0
+    #number of iterations
+    iterations = 1000
+
+    #hyperparameters
+    learning_rate = 0.0001
+    # [b, m] = gradient_descent()
+
+if __name__ == '__main__':
+    main()
