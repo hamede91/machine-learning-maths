211047b7f1abe77310f67071e65c41778b40be65
Alhamede Emad Eldin <alhamede.abdelgadir@gmail.com>
2017-08-19 15:38:37 +0400

    Completed gradient descent program

--- gradient-descent/grad_decent.py
+++ gradient-descent/grad_decent.py
@@ -1,4 +1,40 @@
+from __future__ import print_function
 from numpy import *
+
+def compute_error(b, m, points):
+    sum_squared_error = 0
+    N = float(len(points))
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        sum_squared_error += (1/N) * (y - ((m * x) + b)) **2
+    return sum_squared_error / float(len(points))
+
+def gradient_step(current_b, current_m, learning_rate, iterations, points):
+    b_gradient = 0
+    m_gradient = 0
+    N = float(len(points))
+
+    for i in range(0, len(points)):
+        x = points[i, 0]
+        y = points[i, 1]
+        b_gradient += (-2/N) * (y - ((current_m * x) + current_b))
+        m_gradient += (-2/N) * x * (y - ((current_m * x) + current_b))
+
+    #update b and m
+    new_b = current_b - (learning_rate * b_gradient)
+    new_m = current_m - (learning_rate * m_gradient)
+
+    return [new_b, new_m]
+
+
+def gradient_descent(initial_b, initial_m, learning_rate, iterations, points):
+    b = initial_b
+    m = initial_m
+
+    for i in range(0, iterations):
+        b, m = gradient_step(b, m, learning_rate, iterations, array(points))
+    return [b, m]
 
 
 def main():
@@ -9,11 +45,17 @@ def main():
     b_init = 0
     m_init = 0
     #number of iterations
-    iterations = 1000
+    num_iterations = 1000
 
-    #hyperparameters
+    #learning rate
     learning_rate = 0.0001
-    # [b, m] = gradient_descent()
+
+    print("Startin gradient descent at b = {}, m = {}, error = {}".format(b_init, m_init, compute_error(b_init, m_init, points)))
+    print("Running...")
+    [b, m] = gradient_descent(b_init, m_init, learning_rate, num_iterations, points)
+    print("After {} iterations, b = {}, m = {}, error = {}".format(num_iterations, b, m, compute_error(b, m, points)))
+
+
 
 if __name__ == '__main__':
     main()
